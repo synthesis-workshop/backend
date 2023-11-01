2c236175fe3de2c172364b827d1e290a5443886e
Ahmed Awad <ahmedelsayed11595@gmail.com>
2023-10-31 21:42:04 -0500

    feat: added link modal

--- models/link.ts
+++ models/link.ts
@@ -3,34 +3,36 @@ import type { ListConfig } from "@keystone-6/core";
 import { text, image } from "@keystone-6/core/fields";
 import type { Lists } from ".keystone/types";
 
-export const ProblemSet: ListConfig<Lists.ProblemSet.TypeInfo<any>, any> = list(
-  {
-    access: {
-      operation: {
-        query: () => true,
-        create: ({ session }) => !!session?.data.isAdmin,
-        update: ({ session }) => !!session?.data.isAdmin,
-        delete: ({ session }) => !!session?.data.isAdmin,
-      },
-    },
-    fields: {
-      title: text({
-        validation: { isRequired: true },
-        isFilterable: true,
-        isOrderable: true,
-      }),
-      link: text({
-        validation: {
-          isRequired: true,
-          match: {
-            regex: new RegExp(
-              /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g,
-            ),
-            explanation: "Link must be a valid URL",
-          },
-        },
-      }),
-      logo: image({ storage: "s3_file_storage" }),
+export const Link: ListConfig<any> = list({
+  access: {
+    operation: {
+      query: () => true,
+      create: ({ session }) => !!session?.data.isAdmin,
+      update: ({ session }) => !!session?.data.isAdmin,
+      delete: ({ session }) => !!session?.data.isAdmin,
     },
   },
-);
+  fields: {
+    title: text({
+      validation: { isRequired: true },
+      isFilterable: true,
+      isOrderable: true,
+    }),
+    link: text({
+      validation: {
+        isRequired: true,
+        match: {
+          regex: new RegExp(
+            /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g,
+          ),
+          explanation: "Link must be a valid URL",
+        },
+      },
+    }),
+    logo: text({
+      validation: { isRequired: true },
+      isFilterable: true,
+      isOrderable: true,
+    }),
+  },
+});
