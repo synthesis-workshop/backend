a69e4a57f584e730214f0a60a6415468ae74298c
andkirsten <and.kirsten@gmail.com>
2023-11-16 10:35:41 +0900

    feat: added stats to the meta model for the hero section

---
+++ migrations/20231115063432_1/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `Meta` ADD COLUMN `guestsCount` INTEGER NULL,
+    ADD COLUMN `subscriberCount` INTEGER NULL,
+    ADD COLUMN `videoCount` INTEGER NULL,
+    ADD COLUMN `viewsCount` INTEGER NULL;
